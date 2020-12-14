1cf927f2f04451ba5adfbd7561388ab541dde048
EC2 Default User <ec2-user@ip-172-31-37-182.ec2.internal>
2020-12-14 08:34:16 +0000

    [add]RSpec

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :book do
-    title { Faker::Lorem.characters(number:5) }
-    body { Faker::Lorem.characters(number:20) }
+    title { Faker::Lorem.characters(number:5) } #文字数5のダミータイトル
+    body { Faker::Lorem.characters(number:20) } #文字数20のダミー内容
     user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :user do
-    name { Faker::Lorem.characters(number:10) }
+  factory :user do      
+    name { Faker::Lorem.characters(number:10) } #文字数10のダミー文章
     email { Faker::Internet.email }
     introduction { Faker::Lorem.characters(number:20) }
     password { 'password' }
